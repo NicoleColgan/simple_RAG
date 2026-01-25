a365c86d66041efd682b2747b40c4e8ec311c8ff
Nicole Colgan <nicole.colgan+hpeprod@hpe.com>
2026-01-25 09:21:57 +0000

    document ingestion

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from pydantic import BaseModel
 import logging
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import io
+from pypdf import PdfReader
 
 # Configure logging
 logging.basicConfig(
@@ -42,30 +44,38 @@ async def ingest(files: list[UploadFile] = File(...)):
     for file in files:
         try:
             content = await file.read()
+            text_content = ""
+            file_name = (file.filename or "").lower()
+            text_splitter = None
 
-            # Only decode if its a text file
-            if file.content_type.startswith("text/"):
+            if file_name.endswith(".pdf"):    # Fallback incase content_type is wrong
+                # Handle pdf
+                pdf_file = io.BytesIO(content)  # Create file-like stream object
+                pdf_reader = PdfReader(pdf_file)    # parse pdf files from stream
+                for page in pdf_reader.pages:
+                    text_content += page.extract_text() or ""
+                logger.info(f"Extracted {len(text_content)} from pdf: {file.filename}")
+                text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=100)
+
+            elif file_name.endswith(".txt"):
+                # why did they check content type exists here but not above
                 text_content = content.decode("utf-8")
-            else:
-                # TODO: Use proper parsers for pdfs, word docs etc
-                # text_content = extract_text_from_pdf_or_doc(content)
-                text_content = ""
-            
-            if text_content:
                 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+            
+            if text_content and text_splitter:
                 res = text_splitter.split_text(text_content)
                 print(f"\nres=\n{res}\n")
-            
-            processed_files.append(file.filename)
+                processed_files.append(file.filename)
+
         except Exception as e:
             logger.error(f"failed to process {file.filename}: {e}")
         
         # TODO: send chunks to embedding + vector db
 
-        return IngestResponse(
-            files_processed=len(processed_files),
-            filenames=processed_files
-        )
+    return IngestResponse(
+        files_processed=len(processed_files),
+        filenames=processed_files
+    )
 
 @app.post("/query")
 async def query(user_query: str):
